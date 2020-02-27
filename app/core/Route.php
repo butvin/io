db354525c96fcf38767910f3a38ba3043a0e29a6
butvin <butvin-dev@gmail.com>
2020-02-28 01:32:16 +0200

    arch#4

--- app/core/Route.php
+++ app/core/Route.php
@@ -10,9 +10,15 @@ class Route
 
     protected string $controller = 'index';
 
-    protected function dispatchUri( string $uri)
+    public function dispatchUri(string $uri)
     {
-
+        $routes = explode('/', $_SERVER['REQUEST_URI']);
+        if (!empty($routes[1])) {
+            $controllerName = $routes[1];
+        }
+        if (!empty($routes[2])) {
+            $actionName = $routes[2];
+        }
     }
 
 }
