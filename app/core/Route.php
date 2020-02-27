7feaf50b9a24778c695e29ee9baad4501452c4fa
butvin <butvin-dev@gmail.com>
2020-02-27 21:04:31 +0200

    arch#2

--- app/core/Route.php
+++ app/core/Route.php
@@ -6,5 +6,13 @@ namespace Butvin\Core;
 
 class Route
 {
+    protected string $action = 'index';
+
+    protected string $controller = 'index';
+
+    protected function parseUri( string $uri)
+    {
+
+    }
 
 }
--- app/core/bootstrap.php
+++ app/core/bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 
-require_once 'DB.php';
+//require_once 'DB.php';
+
+$runApplication = new \Butvin\Core\Route();
 
 $conn = \Butvin\Core\DB::getInstance();
 if ($conn) {
-    var_dump($conn);
+    $conn->prepare("SHOW TABLES;");
+
+    var_dump($_SERVER['REQUEST_URI']);
+
+    var_dump(PDO::getAvailableDrivers());
 }
