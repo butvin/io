ad0eccd1b32d56903c5761020680bfe029fba68c
butvin <butvin-dev@gmail.com>
2020-02-28 15:40:49 +0200

    twig intergration #3

--- app/core/BaseController.php
+++ app/core/BaseController.php
@@ -3,18 +3,15 @@
 
 namespace Butvin\Core;
 
-use Butvin\Core\BaseView;
-use Butvin\Core\BaseModel;
-
 abstract class BaseController
 {
     protected BaseView $view;
     protected BaseModel $model;
 
-    public function __construct(BaseView $view, BaseModel$model)
+    public function __construct()
     {
-        $this->view = $view;
-        $this->model = $model;
+        $this->view = new BaseView();
+        $this->model = new BaseModel();
     }
 
     abstract public function indexAction();
--- app/http/Controllers/TicketController.php
+++ app/http/Controllers/TicketController.php
@@ -4,9 +4,19 @@
 namespace Butvin\Controllers;
 
 use \Butvin\Core\BaseController;
+use \Butvin\Core\BaseView;
+use \Butvin\Core\BaseModel;
 
 class TicketController extends BaseController
 {
+    protected BaseView $view;
+    protected BaseModel $model;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     public function indexAction()
     {
         // TODO: Implement indexAction() method.
