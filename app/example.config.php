b1b5da2cfb9e40d4dbf0c382045f553a22b7189a
butvin <butvin-dev@gmail.com>
2020-03-24 17:32:12 +0200

    twig templating-1.1

---
+++ app/example.config.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+/**
+ * DB connection constants
+ */
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'bj');
+define('DB_USER', 'butvin');
+define('DB_PASS', 'qweqwe');
+define('DB_CHAR', 'utf8');
+
+define ('MYSQL_CONN_DEV', 'mysql://'.DB_USER.':'.DB_PASS.'@'.DB_HOST.'/'.DB_NAME.'?charset=utf8');
+//define ('MYSQL_CONN_PROD', 'mysql://'.DB_USER.':'.DB_PASS.'@'.DB_HOST.'/'.DB_NAME);
+
+/**
+ * Filesystem constants
+ */
+define('ROOT_DIR', dirname(__DIR__));
+define('VIEWS_DIR', ROOT_DIR.'/resources/Views');
+define('MODELS_DIR', ROOT_DIR.'/app/http/Models');
+define('CONTROLLERS_DIR', ROOT_DIR.'/app/http/Controllers');
+
+
+define('APP_DIR', __DIR__.'/http');
+define('CORE_DIR', __DIR__.'/core');
+
